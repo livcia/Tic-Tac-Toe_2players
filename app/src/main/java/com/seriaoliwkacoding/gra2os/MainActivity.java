5b67e25bfde5bdefab9054be381b583b1f698d24
Oliwka Ankiewicz <xoliwkaa69@gmail.com>
2021-11-28 13:08:57 +0100

    zrobiona dokumentacja

--- app/src/main/java/com/seriaoliwkacoding/gra2os/MainActivity.java
+++ app/src/main/java/com/seriaoliwkacoding/gra2os/MainActivity.java
@@ -65,22 +65,22 @@ public class MainActivity extends AppCompatActivity {
             ░▀░▀░▀▀▀░▀▀▀░▀▀▀░░░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀░
  */
     public void opcje(int i){
-        TextView zmienna = findViewById(R.id.zmiennanapis);
-        if(!czyWygrana) {
-            if (!kogoKolej){
-                kogoKolej = true;
-                tablica[i].setText("x");
-                checkTablica[i] = true;
-                tablica[i].setEnabled(false);
-                zmienna.setText("O");
-                czywygrana();
+        TextView zmienna = findViewById(R.id.zmiennanapis); //utworzenie informacyjnej zmiennej - kogo kolej
+        if(!czyWygrana) { //tylko jeśli wygrana jest false (gra sie toczy)
+            if (!kogoKolej){ //jeśli zmienna kogoKolej jest false to:
+                kogoKolej = true; //zmien zmienna kogoKolej na true
+                tablica[i].setText("x"); //zaznaczony przycisk wypelnij X
+                checkTablica[i] = true; //zaznacz w tablicy ze dany element jest juz zaznaczony(wartosc true)
+                tablica[i].setEnabled(false); //ustaw dany przycisk na zablokowany
+                zmienna.setText("O"); //zmien informacje kogo kolej na O
+                czywygrana(); //sprawdzanie wygranej
             }else if(kogoKolej){
-                kogoKolej = false;
-                tablica[i].setText("o");
-                checkTablica[i] = true;
-                tablica[i].setEnabled(false);
-                zmienna.setText("X");
-                czywygrana();
+                kogoKolej = false; //zmien zmienna kogoKolej na false
+                tablica[i].setText("o"); //zaznaczony przycisk wypelnij O
+                checkTablica[i] = true; //zaznacz w tablicy ze dany element jest juz zaznaczony(wartosc true)
+                tablica[i].setEnabled(false); //ustaw dany przycisk na zablokowany
+                zmienna.setText("X"); //zmien informacje kogo kolej na X
+                czywygrana(); //sprawdzanie wygranej
             }
         }
     }
@@ -90,6 +90,11 @@ public class MainActivity extends AppCompatActivity {
             ░▀▀▀░▀▀▀░░▀░░░░▀░▀░▀▀▀░▀░▀
  */
     public void czywygrana() {
+        /*
+        ░█▀▄░█▀▀░█░█░█░░░█▀█░█▀▄░█▀█░█▀▀░▀▀█░█▀█░░░▀▀█░█▄█░▀█▀░█▀▀░█▀█░█▀█░█░█░█▀▀░█░█
+        ░█░█░█▀▀░█▀▄░█░░░█▀█░█▀▄░█▀█░█░░░░░█░█▀█░░░▄▀░░█░█░░█░░█▀▀░█░█░█░█░░█░░█░░░█▀█
+        ░▀▀░░▀▀▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀░▀▀░░▀░▀░░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀░▀░░▀░░▀▀▀░▀░▀
+         */
         TextView text = findViewById(R.id.zwyklynapis);
         TextView zmiennanapis = findViewById(R.id.zmiennanapis);
         String b1 = tablica[0].getText().toString();
@@ -101,23 +106,23 @@ public class MainActivity extends AppCompatActivity {
         String b7 = tablica[6].getText().toString();
         String b8 = tablica[7].getText().toString();
         String b9 = tablica[8].getText().toString();
-        if (!czyWygrana) {
+        if (!czyWygrana) { //jesli gra nadal sie toczy to sprawdzaj czy juz przypadkiem sie nie powinna skonczyc (ktos wygra)
             if (checkTablica[0] && b1 == b2 && b1 == b3) { //poziome 1
-                ify(b1);
+                ify(b1); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             } else if (checkTablica[3] && b4 == b5 && b4 == b6) { //poziome 2
-                ify(b4);
+                ify(b4); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             } else if (checkTablica[6] && b7 == b8 && b7 == b9) { //poziome 3
-                ify(b7);
+                ify(b7); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             } else if (checkTablica[0] && b1 == b4 && b1 == b7) { //pionowe 1
-                ify(b7);
+                ify(b7); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             } else if (checkTablica[1] && b2 == b5 && b2 == b8) { //pionowe 2
-                ify(b2);
+                ify(b2); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             } else if (checkTablica[2] && b3 == b6 && b3 == b9) { //pionowe 3
-                ify(b3);
+                ify(b3); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             }else if (checkTablica[0] && b1 == b5 && b1 == b9){ //ukos 1
-                ify(b1);
+                ify(b1); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             }else if (checkTablica[2] && b3 == b5 && b3 == b7){ //ukos 2
-                ify(b3);
+                ify(b3); //przeslanie zawartosci przycisku do ifa który zapisuje w bazie danych i informuje o wygranej zwycięzce
             }
         }
     }
@@ -127,16 +132,16 @@ public class MainActivity extends AppCompatActivity {
             ░▀▀░░▀░▀░▀▀▀░▀░▀░░░▀▀░░▀░▀░▀░▀░░▀░░▀▀▀░▀░▀
  */
     public void ify(String b) { //wykonuje się tylko jak win == true, przesyła do bazy danych kto wygrał, aby w następnej grze zaczynała ta osoba
-        TextView text = findViewById(R.id.zwyklynapis);
-        TextView zmiennanapis = findViewById(R.id.zmiennanapis);
-        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
-        czyWygrana = true;
-        text.setText("Wygrywa gracz: ");
-        zmiennanapis.setText(b);
-        if (b == "o"){
-            prefs.edit().putBoolean("ktoZaczyna", true).apply();
-        }else if (b == "x"){
-            prefs.edit().putBoolean("ktoZaczyna", false).apply();
+        TextView text = findViewById(R.id.zwyklynapis); //dekalracja zmiennej TextView z informacją o zwycięzcy (stała "Wygrywa gracz: ")
+        TextView zmiennanapis = findViewById(R.id.zmiennanapis); //deklaracja zmiennej TextView z informacja kto wygrał
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE); //deklaracja preferencji (baza danych)
+        czyWygrana = true; //zmiana zmiennej czyWygrana na true
+        text.setText("Wygrywa gracz: "); //ustawienie zmiennej na napis z informacją o wygranej
+        zmiennanapis.setText(b); //ustawienie zmiennej na napis z informacją kto wygrał
+        if (b == "o"){ //jeśli przesłana zawartość to "o" to:
+            prefs.edit().putBoolean("ktoZaczyna", true).apply(); //przeslij do bazy danych wartosc true
+        }else if (b == "x"){//jeśli przesłana zawartość to "x" to:
+            prefs.edit().putBoolean("ktoZaczyna", false).apply();//przeslij do bazy danych wartosc false
         }
     }
 /*
@@ -155,14 +160,16 @@ public class MainActivity extends AppCompatActivity {
     public void wypelnij9(View view) { opcje(8); }
 
     public void restart(View view) {
-        TextView text = findViewById(R.id.zwyklynapis);
-        TextView zmienna = findViewById(R.id.zmiennanapis);
+        /*
+WYPEŁNIENIE TABLICY SPRAWDZAJĄCEJ ZAZNACZENIE NA FAlSE (NIE ZAZNACZONE), USTAWIENIE PRZYCISKÓW NA ODBLOKOWANE ORAZ USTAWIENIE ZAWARTOŚCI NA PUSTE
+         */
         for (int i = 0; i < 9;i++){
             checkTablica[i] = false;
             tablica[i].setEnabled(true);
             tablica[i].setText("");
-            czyWygrana = false;
+
         }
-        ktoZaczyna();
+        czyWygrana = false; //ustawienie zmiennej boolean wygrana na false (gra sie toczy)
+        ktoZaczyna(); //funkcja która sprawdza kto powinien zacząć
     }
 }
